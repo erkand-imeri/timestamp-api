8a3224b16059889396a8a1e90667360f095fbb81
Erkand Imeri <erkand.imeri@gmail.com>
2016-11-20 08:24:15 +0000

    initial commit

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
+var fs=require('fs');
+var path=require('path');
+
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  
+  var fileName=path.join(__dirname, 'index.html');
+  
+  res.sendFile(fileName, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+  
 });
 
 app.listen(process.env.PORT || 8080, function () {
